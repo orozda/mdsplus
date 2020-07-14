a18b336bb60e91f19b1ea969cb5631634ed3c16f
Mdsplus <mdsplus@saserver.nbtf>
2020-07-14 09:24:36 +0200

    Fix:missing check

--- mdsobjects/cpp/mdsdatastreams.cpp
+++ mdsobjects/cpp/mdsdatastreams.cpp
@@ -475,6 +475,7 @@ EXPORT void EventStream::send(int shot, const char *name, Data *timesD, Data *sa
 
 EXPORT void EventStream::run()
 {
+    pthread_mutex_lock(&mutex);
     const char *eventName = getName(); //Get the name of the event
     if(strcmp(eventName, "STREAMING")) return; //Should neve return
     size_t bufSize;
@@ -489,6 +490,7 @@ EXPORT void EventStream::run()
     if(readItems < 4)
     {
 	delete [] str;
+	pthread_mutex_unlock(&mutex);
 	return; //Incorrect message
     }
     //skip to fourth blank
@@ -501,6 +503,7 @@ EXPORT void EventStream::run()
 	if(j == len)
 	{
 	    delete [] str;
+	    pthread_mutex_unlock(&mutex);
 	    return; //Incorrect message
 	}
 	j++;
@@ -518,6 +521,7 @@ EXPORT void EventStream::run()
 	    {
 		delete [] times;
 		delete [] str;
+		pthread_mutex_unlock(&mutex);
 		return; //Incorrect message
 	    }
 	    j++;
@@ -540,6 +544,7 @@ EXPORT void EventStream::run()
 	    {
 		delete [] times;
 		delete [] str;
+		pthread_mutex_unlock(&mutex);
 		return; //Incorrect message
 	    }
 	    j++;
@@ -578,6 +583,7 @@ EXPORT void EventStream::run()
 	    delete [] samples;
 	    delete [] str;
 	    deleteData(timesD);
+	    pthread_mutex_unlock(&mutex);
 	    return; //Incorrect message
 	}
 	j++;
@@ -597,11 +603,15 @@ EXPORT void EventStream::run()
     }
     deleteData(samplesD);
     deleteData(timesD);
+    pthread_mutex_unlock(&mutex);
 }
 
 
 EXPORT void EventStream::registerListener(DataStreamListener *listener, const char *name)
 {
+    pthread_mutex_lock(&mutex);
     listeners.push_back(listener);
     names.push_back(std::string(name));
+    pthread_mutex_unlock(&mutex);
+
 }
